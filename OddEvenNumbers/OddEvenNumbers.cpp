8dd0034f36127846a398117321e4851b715bed57
jakey-1 <jakehadady1@gmail.com>
2023-10-21 20:58:13 +0300

    initial commit

---
+++ OddEvenNumbers/OddEvenNumbers.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+void PrintNumbers(string title, const vector<int>& numbers)
+{
+    cout << title << "[ ";
+    for (const auto& num : numbers)
+    {
+        cout << num << " ";
+    }
+    cout << "]\n";
+}
+
+bool IsEven(int number)
+{
+    return number % 2 == 0;
+}
+
+bool IsOdd(int number)
+{
+    return number % 2 != 0;
+}
+
+int main()
+{
+    int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    const int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    vector<int> evenNumbers;
+    vector<int> oddNumbers;
+
+    copy_if(numbers, numbers + size, back_inserter(evenNumbers), IsEven);
+    copy_if(numbers, numbers + size, back_inserter(oddNumbers), IsOdd);
+
+    PrintNumbers("All Numbers: ", vector<int>(numbers, numbers + size));
+    PrintNumbers("Even Numbers: ", evenNumbers);
+    PrintNumbers("Odd Numbers: ", oddNumbers);
+
+    return 0;
+}
